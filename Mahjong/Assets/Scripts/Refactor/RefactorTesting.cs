362316a5f317ff98f69d992c2c57a35a2611d0bc
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2026-01-08 21:49:27 -0800

    Test to build a wall

--- Mahjong/Assets/Scripts/Refactor/RefactorTesting.cs
+++ Mahjong/Assets/Scripts/Refactor/RefactorTesting.cs
@@ -1,20 +1,42 @@
 using UnityEngine;
 using MJ2.Core.Tiles;
 using System.Collections.Generic;
+using MJ.Input;
 
 namespace MJ2.RefactorTesting
 {
     public class RefactorTesting : MonoBehaviour
     {
         [SerializeField] private TileSetSO tileSetSO;
+        [SerializeField] private GameObject tileContainer;
+        [SerializeField] private GameObject tilePrefab;
+        [SerializeField] private InputReader inputReader;
+
+        private List<Tile> tiles;
+
+        private void Awake() {
+            inputReader.EnableDebugInput();
+        }
 
         private void Start() {
             TileSetFactory factory = new TileSetFactory(tileSetSO);
-            List<Tile> tiles = factory.CreateTileSet();
+            tiles = factory.CreateTileSet();
             // factory.ShuffleTiles(tiles);
+        }
 
-            foreach (Tile tile in tiles) {
-                Debug.Log($"{tile.tileType.ToString()} - {tile.tileType.ToCompactString()}");
+        private void OnEnable() {
+            inputReader.startNewGameEvent += OnStartNewGame;
+        }
+
+        private void OnDisable() {
+            inputReader.startNewGameEvent -= OnStartNewGame;
+        }
+
+        private void OnStartNewGame() {
+            int tilesToDeal = 5;
+
+            for (int i = 0; i < tilesToDeal; i++) {
+                Instantiate(tilePrefab, tileContainer.transform);
             }
         }
     }
