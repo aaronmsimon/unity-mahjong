fd047681b1332509ece737f20062fd4b5f73ee2e
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2026-01-27 18:01:20 -0800

    Complete TileDefinition

--- Mahjong/Assets/Scripts/Core/Tiles/TileDefinition.cs
+++ Mahjong/Assets/Scripts/Core/Tiles/TileDefinition.cs
@@ -5,8 +5,9 @@ namespace MJ.Core.Tiles
     [CreateAssetMenu(fileName = "New TileDefinition", menuName = "Mahjong/Tiles/Tile Definition")]
     public class TileDefinition : ScriptableObject
     {
-        public TileID tileInfo;
+        public TileID TileInfo;
         public Sprite Sprite;
         public string DisplayName;
+        public int Copies;
     }
 }
